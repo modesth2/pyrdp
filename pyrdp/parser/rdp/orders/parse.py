bfd1964f680975c2b289ccf21ab4db504783cfbf
Alexandre Beaulieu <alex@segfault.me>
2020-02-28 12:22:57 -0500

    refactor: Made parsing sub-methods private.

--- pyrdp/parser/rdp/orders/parse.py
+++ pyrdp/parser/rdp/orders/parse.py
@@ -92,122 +92,122 @@ class OrdersParser:
 
         numberOrders = Uint16LE.unpack(s)
         for _ in range(numberOrders):
-            self.parse_order(s)
+            self._parse_order(s)
 
         return orders
 
-    def parse_order(self, s: BytesIO):
+    def _parse_order(self, s: BytesIO):
         controlFlags = Uint8.unpack(s)
 
         if not (controlFlags & ControlFlags.TS_STANDARD):
             print('Type: ALTSEC')  # DEBUG
-            self.parse_altsec(s, controlFlags)
+            self._parse_altsec(s, controlFlags)
         elif (controlFlags & ControlFlags.TS_SECONDARY):
             print('Type: SECONDARY')  # DEBUG
-            self.parse_secondary(s, controlFlags)
+            self._parse_secondary(s, controlFlags)
         else:
             print('Type: PRIMARY')  # DEBUG
-            self.parse_primary(s, controlFlags)
+            self._parse_primary(s, controlFlags)
 
     # Primary drawing orders.
     # ----------------------------------------------------------------------
-    def parse_primary(self, s: BytesIO, flags: int):
+    def _parse_primary(self, s: BytesIO, flags: int):
         if flags & ControlFlags.TS_TYPE_CHANGE:
             orderType = Uint8.unpack(s)
 
         read_field_flags(s)  # TODO:
 
-    def parse_dstblt(self, s: BytesIO):
+    def _parse_dstblt(self, s: BytesIO):
         """DSTBLT"""
         pass
 
-    def parse_patblt(self, s: BytesIO):
+    def _parse_patblt(self, s: BytesIO):
         """PATBLT"""
         pass
 
-    def parse_scrblt(self, s: BytesIO):
+    def _parse_scrblt(self, s: BytesIO):
         """SCRBLT"""
         pass
 
-    def parse_draw_nine_grid(self, s: BytesIO):
+    def _parse_draw_nine_grid(self, s: BytesIO):
         """DRAW_NINE_GRID"""
         pass
 
-    def parse_multi_draw_nine_grid(self, s: BytesIO):
+    def _parse_multi_draw_nine_grid(self, s: BytesIO):
         """MULTI_DRAW_NINE_GRID"""
         pass
 
-    def parse_line_to(self, s: BytesIO):
+    def _parse_line_to(self, s: BytesIO):
         """LINE_TO"""
         pass
 
-    def parse_opaque_rect(self, s: BytesIO):
+    def _parse_opaque_rect(self, s: BytesIO):
         """OPAQUE_RECT"""
         pass
 
-    def parse_save_bitmap(self, s: BytesIO):
+    def _parse_save_bitmap(self, s: BytesIO):
         """SAVE_BITMAP"""
         pass
 
-    def parse_memblt(self, s: BytesIO):
+    def _parse_memblt(self, s: BytesIO):
         """MEMBLT"""
         pass
 
-    def parse_mem3blt(self, s: BytesIO):
+    def _parse_mem3blt(self, s: BytesIO):
         """MEM3BLT"""
         pass
 
-    def parse_multi_dstblt(self, s: BytesIO):
+    def _parse_multi_dstblt(self, s: BytesIO):
         """MULTI_DSTBLT"""
         pass
 
-    def parse_multi_patblt(self, s: BytesIO):
+    def _parse_multi_patblt(self, s: BytesIO):
         """MULTI_PATBLT"""
         pass
 
-    def parse_multi_scrblt(self, s: BytesIO):
+    def _parse_multi_scrblt(self, s: BytesIO):
         """MULTI_SCRBLT"""
         pass
 
-    def parse_multi_opaque_rect(self, s: BytesIO):
+    def _parse_multi_opaque_rect(self, s: BytesIO):
         """MULTI_OPAQUE_RECT"""
         pass
 
-    def parse_fast_index(self, s: BytesIO):
+    def _parse_fast_index(self, s: BytesIO):
         """FAST_INDEX"""
         pass
 
-    def parse_polygon_sc(self, s: BytesIO):
+    def _parse_polygon_sc(self, s: BytesIO):
         """POLYGON_SC"""
         pass
 
-    def parse_polygon_cb(self, s: BytesIO):
+    def _parse_polygon_cb(self, s: BytesIO):
         """POLYGON_CB"""
         pass
 
-    def parse_polyline(self, s: BytesIO):
+    def _parse_polyline(self, s: BytesIO):
         """POLYLINE"""
         pass
 
-    def parse_fast_glyph(self, s: BytesIO):
+    def _parse_fast_glyph(self, s: BytesIO):
         """FAST_GLYPH"""
         pass
 
-    def parse_ellipse_sc(self, s: BytesIO):
+    def _parse_ellipse_sc(self, s: BytesIO):
         """ELLIPSE_SC"""
         pass
 
-    def parse_ellipse_cb(self, s: BytesIO):
+    def _parse_ellipse_cb(self, s: BytesIO):
         """ELLIPSE_CB"""
         pass
 
-    def parse_glyph_index(self, s: BytesIO):
+    def _parse_glyph_index(self, s: BytesIO):
         """GLYPH_INDEX"""
         pass
 
     # Secondary drawing orders.
     # ----------------------------------------------------------------------
-    def parse_secondary(self, s: BytesIO, flags: int):
+    def _parse_secondary(self, s: BytesIO, flags: int):
         orderLength = Uint16LE.unpack(s)
         extraFlags = Uint16LE.unpack(s)  # TODO: Need to pass these through as well.
         orderType = Uint8.unpack(s)
@@ -219,7 +219,7 @@ class OrdersParser:
         print(f'Order: {_repr(fp)}')  # DEBUG
         fp(self, s, orderType, extraFlags)
 
-    def parse_cache_bitmap_v1(self, s: BytesIO, orderType: int, flags: int):
+    def _parse_cache_bitmap_v1(self, s: BytesIO, orderType: int, flags: int):
         """CACHE_BITMAP_V1"""
         cacheId = Uint8.unpack(s)
         s.read(1)  # Padding
@@ -237,7 +237,7 @@ class OrdersParser:
 
         data = s.read(bitmapLength)
 
-    def parse_cache_color_table(self, s: BytesIO, orderType: int, flags: int):
+    def _parse_cache_color_table(self, s: BytesIO, orderType: int, flags: int):
         """CACHE_COLOR_TABLE"""
         cacheIndex = Uint8.unpack(s)
         numberColors = Uint16LE.unpack(s)
@@ -245,7 +245,7 @@ class OrdersParser:
         assert numberColors == 256
         colors = [read_color(s) for _ in range(numberColors)]
 
-    def parse_cache_glyph(self, s: BytesIO, orderType: int, flags: int):
+    def _parse_cache_glyph(self, s: BytesIO, orderType: int, flags: int):
         """CACHE_GLYPH"""
         # 2.2.2.2.1.2.5
         # FIXME: Need to know from capabilities whether the server is sending V1 or V2 glyph caches.
@@ -269,7 +269,7 @@ class OrdersParser:
 
         # onGlyphUnicodeChars
 
-    def parse_cache_bitmap_v2(self, s: BytesIO, orderType: int, flags: int):
+    def _parse_cache_bitmap_v2(self, s: BytesIO, orderType: int, flags: int):
         """CACHE_BITMAP_V2"""
         # 2.2.2.2.1.2.3
         cacheId = flags & 0x0003
@@ -306,7 +306,7 @@ class OrdersParser:
         data = s.read(bitmapLength)
         print(data)  # DEBUG
 
-    def parse_cache_brush(self, s: BytesIO, orderType: int, flags: int):
+    def _parse_cache_brush(self, s: BytesIO, orderType: int, flags: int):
         """CACHE_BRUSH"""
         # 2.2.2.2.1.2.7
         cacheIndex = Uint8.unpack(s)
@@ -342,7 +342,7 @@ class OrdersParser:
                 o = (7-i)*scanline
                 data[o:o+8] = s.read(scanline)
 
-    def parse_cache_bitmap_v3(self, s: BytesIO, orderType: int, flags: int):
+    def _parse_cache_bitmap_v3(self, s: BytesIO, orderType: int, flags: int):
         """CACHE_BITMAP_V3"""
         cacheId = flags & 0x00000003
         flags = (flags & 0x0000FF80) >> 7
@@ -363,7 +363,7 @@ class OrdersParser:
 
     # Alternate secondary drawing orders.
     # ----------------------------------------------------------------------
-    def parse_altsec(self, s: BytesIO, flags: int):
+    def _parse_altsec(self, s: BytesIO, flags: int):
         orderType = flags >> 2
 
         # TODO: Log unsupported orders.
@@ -373,7 +373,7 @@ class OrdersParser:
         print(f'Order: {_repr(fp)}')  # DEBUG
         fp(self, s)
 
-    def parse_create_offscreen_bitmap(self, s: BytesIO):
+    def _parse_create_offscreen_bitmap(self, s: BytesIO):
         """CREATE_OFFSCREEN_BITMAP"""
         flags = Uint16LE.unpack(s)
         bitmapId = flags & 0x7FFF
@@ -390,11 +390,11 @@ class OrdersParser:
                 i = Uint16LE.unpack(s)
                 # TODO: Delete bitmap from cache.
 
-    def parse_switch_surface(self, s: BytesIO):
+    def _parse_switch_surface(self, s: BytesIO):
         """SWITCH_SURFACE"""
         surfaceId = Uint16LE.unpack(s)
 
-    def parse_create_nine_grid_bitmap(self, s: BytesIO):
+    def _parse_create_nine_grid_bitmap(self, s: BytesIO):
         """CREATE_NINEGRID_BITMAP"""
         bpp = Uint8.unpack(s)
         bmpId = Uint16LE.unpack(s)
@@ -407,7 +407,7 @@ class OrdersParser:
         rgb = colorref(s)
         # TODO: Allocate the bitmap entry
 
-    def parse_stream_bitmap_first(self, s: BytesIO):
+    def _parse_stream_bitmap_first(self, s: BytesIO):
         """STREAM_BITMAP_FIRST"""
         flags = Uint8.unpack(s)
         bpp = Uint8.unpack(s)
@@ -426,13 +426,13 @@ class OrdersParser:
 
         blockSize = skip16(s)
 
-    def parse_stream_bitmap_next(self, s: BytesIO):
+    def _parse_stream_bitmap_next(self, s: BytesIO):
         """STREAM_BITMAP_NEXT"""
         flags = Uint8.unpack(s)
         bitmapType = Uint16LE.unpack(s)
         blockSize = skip16(s)
 
-    def parse_gdiplus_first(self, s: BytesIO):
+    def _parse_gdiplus_first(self, s: BytesIO):
         """GDIPLUS_FIRST"""
         s.read(1)  # Padding
         cbSize = Uint16LE.unpack(s)  # TODO: Store cbSize
@@ -440,12 +440,12 @@ class OrdersParser:
         cbTotalEmfSize = Uint32LE.unpack(s)
         emf = s.read(cbSize)
 
-    def parse_gdiplus_next(self, s: BytesIO):
+    def _parse_gdiplus_next(self, s: BytesIO):
         """GDIPLUS_NEXT"""
         s.read(1)  # Padding
         emf = s.read(cbSize)  # TODO: Get cbSize from context
 
-    def parse_gdiplus_end(self, s: BytesIO):
+    def _parse_gdiplus_end(self, s: BytesIO):
         """GDIPLUS_END"""
         s.read(1)  # Padding
         cbSize = Uint16LE.unpack(s)
@@ -453,7 +453,7 @@ class OrdersParser:
         cbTotalEmfSize = Uint32LE.unpack(s)
         emf = s.read(cbSize)  # TODO: Get cbSize from context
 
-    def parse_gdiplus_cache_first(self, s: BytesIO):
+    def _parse_gdiplus_cache_first(self, s: BytesIO):
         """GDIPLUS_CACHE_FIRST"""
         flags = Uint8.unpack(s)
         cacheType = Uint16LE.unpack(s)
@@ -462,14 +462,14 @@ class OrdersParser:
         cbTotalSize = Uint32LE.unpack(s)
         emf = s.read(cbSize)
 
-    def parse_gdiplus_cache_next(self, s: BytesIO):
+    def _parse_gdiplus_cache_next(self, s: BytesIO):
         """GDIPLUS_CACHE_NEXT"""
         flags = Uint8.unpack(s)
         cacheType = Uint16LE.unpack(s)
         cacheIndex = Uint16LE.unpack(s)
         emf = s.read(cbSize)
 
-    def parse_gdiplus_cache_end(self, s: BytesIO):
+    def _parse_gdiplus_cache_end(self, s: BytesIO):
         """GDIPLUS_CACHE_END"""
         flags = Uint8.unpack(s)
         cacheType = Uint16LE.unpack(s)
@@ -478,16 +478,16 @@ class OrdersParser:
         cbTotalSize = Uint32LE.unpack(s)
         emf = s.read(cbSize)
 
-    def parse_window(self, s: BytesIO):
+    def _parse_window(self, s: BytesIO):
         """WINDOW"""
         # This is specified in MS-RDPERP for seamless applications.
         LOG.debug('WINDOW is not supported yet.')
 
-    def parse_compdesk_first(self, s: BytesIO):
+    def _parse_compdesk_first(self, s: BytesIO):
         """COMPDESK"""
         LOG.debug('COMPDESK is not supported yet.')
 
-    def parse_frame_marker(self, s: BytesIO):
+    def _parse_frame_marker(self, s: BytesIO):
         """FRAME_MARKER"""
         action = Uint32LE.unpack(s)
 
@@ -497,67 +497,67 @@ class OrdersParser:
 
 # Parser Lookup Tables
 _pri = [
-    OrdersParser.parse_dstblt,  # 0x00
-    OrdersParser.parse_patblt,  # 0x01
-    OrdersParser.parse_scrblt,  # 0x02
-    None,  # 0x03
-    None,  # 0x04
-    None,  # 0x05
-    None,  # 0x06
-    OrdersParser.parse_draw_nine_grid,  # 0x07
-    OrdersParser.parse_multi_draw_nine_grid,  # 0x08
-    OrdersParser.parse_line_to,  # 0x09
-    OrdersParser.parse_opaque_rect,  # 0x0A
-    OrdersParser.parse_save_bitmap,  # 0x0B
-    None,  # 0x0C
-    OrdersParser.parse_memblt,  # 0x0D
-    OrdersParser.parse_mem3blt,  # 0x0E
-    OrdersParser.parse_multi_dstblt,  # 0x0F
-    OrdersParser.parse_multi_patblt,  # 0x10
-    OrdersParser.parse_multi_scrblt,  # 0x11
-    OrdersParser.parse_multi_opaque_rect,  # 0x12
-    OrdersParser.parse_fast_index,  # 0x13
-    OrdersParser.parse_polygon_sc,  # 0x14
-    OrdersParser.parse_polygon_cb,  # 0x15
-    OrdersParser.parse_polyline,  # 0x16
-    None,
-    OrdersParser.parse_fast_glyph,  # 0x18
-    OrdersParser.parse_ellipse_sc,  # 0x19
-    OrdersParser.parse_ellipse_cb,  # 0x1A
-    OrdersParser.parse_glyph_index,  # 0x1B
+    OrdersParser._parse_dstblt,                # 0x00
+    OrdersParser._parse_patblt,                # 0x01
+    OrdersParser._parse_scrblt,                # 0x02
+    None,                                      # 0x03
+    None,                                      # 0x04
+    None,                                      # 0x05
+    None,                                      # 0x06
+    OrdersParser._parse_draw_nine_grid,        # 0x07
+    OrdersParser._parse_multi_draw_nine_grid,  # 0x08
+    OrdersParser._parse_line_to,               # 0x09
+    OrdersParser._parse_opaque_rect,           # 0x0A
+    OrdersParser._parse_save_bitmap,           # 0x0B
+    None,                                      # 0x0C
+    OrdersParser._parse_memblt,                # 0x0D
+    OrdersParser._parse_mem3blt,               # 0x0E
+    OrdersParser._parse_multi_dstblt,          # 0x0F
+    OrdersParser._parse_multi_patblt,          # 0x10
+    OrdersParser._parse_multi_scrblt,          # 0x11
+    OrdersParser._parse_multi_opaque_rect,     # 0x12
+    OrdersParser._parse_fast_index,            # 0x13
+    OrdersParser._parse_polygon_sc,            # 0x14
+    OrdersParser._parse_polygon_cb,            # 0x15
+    OrdersParser._parse_polyline,              # 0x16
+    None,                                      # 0x17
+    OrdersParser._parse_fast_glyph,            # 0x18
+    OrdersParser._parse_ellipse_sc,            # 0x19
+    OrdersParser._parse_ellipse_cb,            # 0x1A
+    OrdersParser._parse_glyph_index,           # 0x1B
 ]
 
 assert len(_pri) == 0x1C
 
 _sec = [
-    OrdersParser.parse_cache_bitmap_v1,  # 0x00 : Uncompressed
-    OrdersParser.parse_cache_color_table,  # 0x01
-    OrdersParser.parse_cache_bitmap_v1,  # 0x02 : Compressed
-    OrdersParser.parse_cache_glyph,  # 0x03
-    OrdersParser.parse_cache_bitmap_v2,  # 0x04 : Uncompresed
-    OrdersParser.parse_cache_bitmap_v2,  # 0x05 : Compressed
-    None,  # 0x06
-    OrdersParser.parse_cache_brush,  # 0x07
-    OrdersParser.parse_cache_bitmap_v3,  # 0x08
+    OrdersParser._parse_cache_bitmap_v1,    # 0x00 : Uncompressed
+    OrdersParser._parse_cache_color_table,  # 0x01
+    OrdersParser._parse_cache_bitmap_v1,    # 0x02 : Compressed
+    OrdersParser._parse_cache_glyph,        # 0x03
+    OrdersParser._parse_cache_bitmap_v2,    # 0x04 : Uncompresed
+    OrdersParser._parse_cache_bitmap_v2,    # 0x05 : Compressed
+    None,                                   # 0x06
+    OrdersParser._parse_cache_brush,        # 0x07
+    OrdersParser._parse_cache_bitmap_v3,    # 0x08
 ]
 
 assert len(_sec) == 0x09
 
 _alt = [
-    OrdersParser.parse_switch_surface,  # 0x00
-    OrdersParser.parse_create_offscreen_bitmap,  # 0x01
-    OrdersParser.parse_stream_bitmap_first,  # 0x02
-    OrdersParser.parse_stream_bitmap_next,  # 0x03
-    OrdersParser.parse_create_nine_grid_bitmap,  # 0x04
-    OrdersParser.parse_gdiplus_first,  # 0x05
-    OrdersParser.parse_gdiplus_next,  # 0x06
-    OrdersParser.parse_gdiplus_end,  # 0x07
-    OrdersParser.parse_gdiplus_cache_first,  # 0x08
-    OrdersParser.parse_gdiplus_cache_next,  # 0x09
-    OrdersParser.parse_gdiplus_cache_end,  # 0x0A
-    OrdersParser.parse_window,  # 0x0B
-    OrdersParser.parse_compdesk_first,  # 0x0C
-    OrdersParser.parse_frame_marker,  # 0x0D
+    OrdersParser._parse_switch_surface,           # 0x00
+    OrdersParser._parse_create_offscreen_bitmap,  # 0x01
+    OrdersParser._parse_stream_bitmap_first,      # 0x02
+    OrdersParser._parse_stream_bitmap_next,       # 0x03
+    OrdersParser._parse_create_nine_grid_bitmap,  # 0x04
+    OrdersParser._parse_gdiplus_first,            # 0x05
+    OrdersParser._parse_gdiplus_next,             # 0x06
+    OrdersParser._parse_gdiplus_end,              # 0x07
+    OrdersParser._parse_gdiplus_cache_first,      # 0x08
+    OrdersParser._parse_gdiplus_cache_next,       # 0x09
+    OrdersParser._parse_gdiplus_cache_end,        # 0x0A
+    OrdersParser._parse_window,                   # 0x0B
+    OrdersParser._parse_compdesk_first,           # 0x0C
+    OrdersParser._parse_frame_marker,             # 0x0D
 ]
 
 assert len(_alt) == 0x0E
